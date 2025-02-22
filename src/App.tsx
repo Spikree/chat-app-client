48341dd7a3ef5aaf1c95a39e96bd099bdfab2476
spikree <avimahalingpure10@gmail.com>
2025-02-22 23:23:34 +0530

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import {useAuthStore} from "./store/useAuthStore.js"
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+
+const App = () => {
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth])
+
+  console.log(authUser);
+
+  if(isCheckingAuth) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+  }
+
+  return <div>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/signUp" element={<SignUpPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/settings" element={<SettingsPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+    </Routes>
+  </div>;
+};
+
+export default App;
