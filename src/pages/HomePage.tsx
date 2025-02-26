e6140a7332f19cf98badd53b63000bfdbda748b1
spikree <avimahalingpure10@gmail.com>
2025-02-26 22:31:30 +0530

    create chat store for user and messages storage and also create home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import Chat from "../components/Chat.tsx";
+import NoChatSelected from "../components/NoChatSelected.tsx";
+import Sidebar from "../components/Sidebar.tsx";
+import { useChatStore } from "../store/useChatStore.ts"
 
-type Props = {}
-
-const HomePage = (props: Props) => {
+const HomePage = () => {
+  const {setSelectedUser} = useChatStore();
   return (
-    <div>HomePage</div>
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar/>
+
+            {setSelectedUser ? <Chat/> : <NoChatSelected/> }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/store/useChatStore.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import axiosInstance from "../lib/axios.ts";
+import toast from "react-hot-toast";
+import { AxiosError } from 'axios';
+
+export const useChatStore = create((set) => ({
+    messages:[],
+    users:[],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () => {
+        set({isUsersLoading: true})
+        try {
+            const response = await axiosInstance.get("/messages/users")
+            set({users: response.data})
+            toast.success(response.data.message)
+        } catch (error) {
+            set({isUserLoading:false})
+            toast.error((error as AxiosError<{message: string}>).response?.data?.message || 'An error occurred');
+        }
+    },
+
+    getMessages: async(userId: string) => {
+        set({isMessagesLoading: true})
+        try {
+            const response = await axiosInstance.get(`/messages/${userId}`);
+            set({
+                messages: response.data.messages
+            })
+        } catch (error) {
+            toast.error((error as AxiosError<{message: string}>).response?.data?.message || 'An error occurred');
+            set({isMessagesLoading: false})
+        } finally {
+            set({isMessagesLoading: false})
+        }
+    },
+}))
